2fb767af7e1b5b9a034850ec32c2fbc1d8714b32
jburge <jburge16@my.whitworth.edu>
2016-04-18 23:07:51 -0700

    modified:   LoginPageViewController.cs

--- Xamarin/WorkoutAnywhere/WorkoutAnywhere/LoginPageViewController.cs
+++ Xamarin/WorkoutAnywhere/WorkoutAnywhere/LoginPageViewController.cs
@@ -1,1 +1,44 @@
-﻿using Foundation; using System; using System.CodeDom.Compiler; using System.Net; using System.Text; using System.IO; using UIKit;  namespace WorkoutAnywhere { 	partial class LoginPageViewController : UIViewController 	{ 		public LoginPageViewController (IntPtr handle) : base (handle) 		{             PasswordText.Placeholder = "Password";             UsernameText.Placeholder = "Username";			 		} 		partial void LoginButtonClick (UIButton sender) 		{ 			tryLogin(); 		} 		partial void tempTest (UIButton sender) 		{ 			tryLogin(); 		} 		public void tryLogin() 		{ 			string username = UsernameText.Text; 			string password = PasswordText.Text; 			HttpWebRequest request = WebRequest.Create ("http://workoutanywhere.net/DatabaseConnection_iOS_App/Login.php?user=('" + username + "')&pass=('" + password + "')") as HttpWebRequest; 			using (HttpWebResponse response = request.GetResponse () as HttpWebResponse) { 			StreamReader reader = new StreamReader (response.GetResponseStream ()); 				string result = reader.ReadLine (); 				//if (Convert.ToInt32(result) == 1) 				//	tempText.Text = "Logged In"; 				//else if (Convert.ToInt32(result) == 0) 				//	tempText.Text = "Log In Failed"; 				//else 				//	tempText.Text = "ERROR"; 				tempText.Text += result; 			} 		}	 	} } 
+﻿using Foundation;
+using System;
+using System.CodeDom.Compiler;
+using System.Net;
+using System.Text;
+using System.IO;
+using UIKit;
+
+namespace WorkoutAnywhere
+{
+	partial class LoginPageViewController : UIViewController
+	{
+		public LoginPageViewController (IntPtr handle) : base (handle)
+		{
+            PasswordText.Placeholder = "Password";
+            UsernameText.Placeholder = "Username";			
+		}
+		partial void LoginButtonClick (UIButton sender)
+		{
+			tryLogin();
+		}
+		partial void tempTest (UIButton sender)
+		{
+			tryLogin();
+		}
+		public void tryLogin()
+		{
+			string username = UsernameText.Text;
+			string password = PasswordText.Text;
+			HttpWebRequest request = WebRequest.Create ("http://workoutanywhere.net/DatabaseConnection_iOS_App/Login.php?user=('" + username + "')&pass=('" + password + "')") as HttpWebRequest;
+			using (HttpWebResponse response = request.GetResponse () as HttpWebResponse) {
+			StreamReader reader = new StreamReader (response.GetResponseStream ());
+				string result = reader.ReadLine ();
+				//if (Convert.ToInt32(result) == 1)
+				//	tempText.Text = "Logged In";
+				//else if (Convert.ToInt32(result) == 0)
+				//	tempText.Text = "Log In Failed";
+				//else
+				//	tempText.Text = "ERROR";
+				tempText.Text += result;
+			}
+		}	
+	}
+}
